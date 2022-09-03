099870ff0ff23294548b0885f0be5222d7c74c74
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-03 16:19:36 +0200

    Update Weathers.js

--- src/components/redux/Weathers.js
+++ src/components/redux/Weathers.js
@@ -17,49 +17,8 @@ export const addCity = (payload) => ({
   payload,
 });
 
-/*
-data.forEach((obj) => {
-  const newObj = {
-    lon: obj.lon,
-    lat: obj.lat,
-    main: obj.weather.main,
-    icon: obj.weather.icon,
-    temp: obj.main.temp,
-    feels_like: obj.main.feels_like,
-    temp_min: obj.main.temp_min,
-    temp_max: obj.main.temp_max,
-    pressure: obj.main.pressure,
-    humidity: obj.main.humidity,
-    speed: obj.wind.speed,
-    deg: obj.wind.deg,
-    gust: obj.wind.gust,
-    all: obj.clouds.all,
-    country: obj.sys.country,
-    sunrise: obj.sys.sunrise,
-    sunset: obj.sys.sunset,
-    timezone: obj.timezone,
-    id: obj.weather.Id,
-    name: obj.name,
-  };
-  changed.push(newObj);
-});
-return changed;
-}
-*/
-
 export const getWeather = () => async (dispatch) => {
   await fetch(API_URL)
     .then((res) => res.json())
     .then((data) => { dispatch(addCity(data.list)); });
 };
-
-/*
-export { getWeather };
- export const getWeathers =  async (dispatch) => {
-const response = await fetch(api)
-const responsefetch = await response.json()
-console.log(responsefetch)
-return responsefetch;
-
- }
-*/
